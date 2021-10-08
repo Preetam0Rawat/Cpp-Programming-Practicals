6bc752af940cb96ee36d28b949abe2fb75824606
Preetam Rawat <91361433+PreetamBoi@users.noreply.github.com>
2021-10-08 10:05:36 +0530

    Update and rename Untitled Document 1.cpp to  datatype 1.cpp

---
+++ Practical-5/task-1/ datatype 1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+  int main()
+  {
+   int a=10;
+   
+   cout<<"size of int is :"<<sizeof(int)<<endl;
+   cout<<"size of a is :"<<sizeof(a)<<endl;
+  
+  return 0;
+} 
+  
--- Practical-5/task-1/Untitled Document 1.cpp
+++
@@ -1,9 +0,0 @@
-include<iostream>
-using namesace std;
-{
-  int main()
-    int a=523457;
-  cout<<"a"<<endl;
-  return 0;
-} 
-  
