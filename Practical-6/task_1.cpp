ce9357d5e9150ddc1f4934b210e60bb367c6af6b
Preetam Rawat <91361433+PreetamBoi@users.noreply.github.com>
2021-10-13 10:31:41 +0530

    Add files via upload

---
+++ Practical-6/task_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a = 2,  b =8;
+   float c = 2.2, d = 3.3;
+   string str1 = "Red";
+   string str2 = "Apple";
+   
+   cout<<"a+b = "<<a+b;   
+   cout<<"c+d = "<<c+d; 
+   cout<<"str1 + str2 = "<<str1+str2;
+   cout<<"String + a = "<<str1 + a; 
+   
+   return 0;
+}
